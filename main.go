498453eccb5804f9aa594c120b9e211363d832ed
Juan Calderon <juanalcarios@gmail.com>
2023-07-22 18:44:48 -0500

    Siguiente commit

--- main.go
+++ main.go
@@ -1,1 +1,46 @@
 package twittergo
+
+import (
+	"context"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+
+func main(){
+	lambda.Start(EjecutoLambda)
+}
+
+func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest)(*events.APIGatewayProxyResponse, error){
+	var res *events.APIGatewayProxyResponse
+
+	if !ValidoParametros(){
+		res = &events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body: "Error en las variables de entorno. Debe incluir 'SecretName', 'BucketName',''UrlPrefix",
+			Headers: map[string]string{
+				"Content-Type": "application/json",
+			},
+		}
+		return res, nil
+	}
+	return res, nil
+}
+
+func ValidoParametros()bool{
+	_, traeParametro := os.LookupEnv("SecretName")
+	if !traeParametro {
+		return traeParametro
+	}
+	_, traeParametro = os.LookupEnv("BucketName")
+	if !traeParametro {
+		return traeParametro
+	}
+	_, traeParametro = os.LookupEnv("UrlPrefix")
+	if !traeParametro {
+		return traeParametro
+	}
+	return traeParametro
+}
