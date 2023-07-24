8911d511504b1830177fd10f900b45f8ed597ece
Juan Calderon <juanalcarios@gmail.com>
2023-07-23 20:45:27 -0500

    Siguiente commit

--- main.go
+++ main.go
@@ -2,49 +2,61 @@ package twittergo
 
 import (
 	"context"
-	"fmt"
 	"os"
+	"strings"
 
 	"github.com/Alejandro9912/twitterGo/awsgo"
+	"github.com/Alejandro9912/twitterGo/models"
 	secretmanager "github.com/Alejandro9912/twitterGo/secretManager"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-
-func main(){
+func main() {
 	lambda.Start(EjecutoLambda)
 }
 
-func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest)(*events.APIGatewayProxyResponse, error){
+func EjecutoLambda(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	var res *events.APIGatewayProxyResponse
 
 	awsgo.InicializoAWS()
 
-	if !ValidoParametros(){
+	if !ValidoParametros() {
 		res = &events.APIGatewayProxyResponse{
 			StatusCode: 400,
-			Body: "Error en las variables de entorno. Debe incluir 'SecretName', 'BucketName',''UrlPrefix",
+			Body:       "Error en las variables de entorno. Debe incluir 'SecretName', 'BucketName',''UrlPrefix",
 			Headers: map[string]string{
 				"Content-Type": "application/json",
 			},
 		}
 		return res, nil
 	}
-	SecretModel,err := secretmanager.GetSecret(os.Getenv("SecretName"))
-	if err !=nil{
+	SecretModel, err := secretmanager.GetSecret(os.Getenv("SecretName"))
+	if err != nil {
 		res = &events.APIGatewayProxyResponse{
 			StatusCode: 400,
-			Body: "Error en la lectura de Secret" + err.Error(),
+			Body:       "Error en la lectura de Secret" + err.Error(),
 			Headers: map[string]string{
 				"Content-Type": "application/json",
 			},
 		}
 		return res, nil
 	}
+
+	path := strings.Replace(request.PathParameters["twitterGo"], os.Getenv("UrlPrefix"), "", -1)
+
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("path"), path)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("method"), request.HTTPMethod)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("user"), SecretModel.UserName)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("password"), SecretModel.Password)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("host"), SecretModel.Host)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("database"), SecretModel.Database)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("jwtSing"), SecretModel.JWTSing)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("body"), request.Body)
+	awsgo.Ctx = context.WithValue(awsgo.Ctx, models.Key("bucketName"), os.Getenv("BucketName"))
 }
 
-func ValidoParametros()bool{
+func ValidoParametros() bool {
 	_, traeParametro := os.LookupEnv("SecretName")
 	if !traeParametro {
 		return traeParametro
